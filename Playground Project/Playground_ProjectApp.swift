d49d1032e98daf3dcd71d09258e53512849baf4c
Manenga Mungandi <manengam@rain.co.za>
2023-07-01 13:31:35 +0200

    Initial Commit

---
+++ Playground Project/Playground_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Playground_ProjectApp.swift
+//  Playground Project
+//
+//  Created by Manenga Mungandi on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct Playground_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
